873063ec22e199cbfcbabf69ccfe83af5fe3092c
malithi19 <malithiranawaka8@gmail.com>
2024-06-18 19:48:43 +0530

    initial commit

---
+++ social/models.py
@@ -0,0 +1,106 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.conf import settings
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='profiles/', null=True, blank=True)
+    cover_picture = models.ImageField(upload_to='covers/', null=True, blank=True)
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=255, blank=True)
+    website = models.URLField(blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Photo(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='photos')
+    image = models.ImageField(upload_to='photos/')
+    caption = models.TextField(blank=True, null=True)
+    tags = models.ManyToManyField(User, related_name='tagged_photos', blank=True)
+    location = models.CharField(max_length=255, blank=True, null=True)
+    uploaded_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'Photo {self.id} by {self.owner.username}'
+
+
+class Comment(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on Post {self.post.id}'
+
+
+class Friendship(models.Model):
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friendships_created')
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friendships_received')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.from_user.username} -> {self.to_user.username}'
+
+
+class Post(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    content = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='post_images', blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked_posts_set', blank=True)
+
+    def __str__(self):
+        return f'Post {self.id} by {self.author.username}'
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_likes')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} likes Post {self.post.id}'
+
+
+class Tag(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    created_at = models.DateTimeField(default=timezone.now)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Tag by {self.user.username} on Post {self.post.id if self.post else "N/A"}'
+
+
+class Feed(models.Model):
+    CONTENT_TYPES = (
+        ('post', 'Post'),
+        ('photo', 'Photo'),
+        ('comment', 'Comment'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='feeds')
+    content_type = models.CharField(max_length=20, choices=CONTENT_TYPES)
+    object_id = models.PositiveIntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Feed item {self.id} for {self.user.username}'
+
+
+class Mention(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    created_at = models.DateTimeField(default=timezone.now)
+    comment = models.ForeignKey('Comment', on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f'Mention by {self.user.username} on Post {self.post.id if self.post else "N/A"} or Comment {self.comment.id if self.comment else "N/A"}'
