de48858281545b46cfe05f40080efc558f2cc7e2
SandaliTharumini <tharuminiwellehewa@gmail.com>
2024-06-21 10:14:12 +0530

    linked sign up

--- social/urls.py
+++ social/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('', views.newsfeed, name='newsfeed'),
     path('', views.reset_password, name='reset_password'),
     path('', views.settings, name='settings'),
-    path('', views.sign_up, name='sign_up'),
+    path('sign-up/', views.sign_up, name='sign_up'),
     path('forgot-password/', views.forgot_password, name='forgot_password'),
 ]
 
