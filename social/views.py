96c3e46dfa84a82d811d1ba58106c5562cfd4baf
malithi19 <malithiranawaka8@gmail.com>
2024-06-23 01:43:21 +0530

    Update sign_up and sign_in

--- social/views.py
+++ social/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
 from rest_framework import viewsets
+
+from .forms import SignUpForm
 from .models import UserProfile, Photo, Comment, Friendship, Post, Like, Tag, Feed
-from .serializers import UserProfileSerializer, PhotoSerializer, CommentSerializer, FriendshipSerializer, PostSerializer, LikeSerializer, TagSerializer, FeedSerializer
+from .serializers import UserProfileSerializer, PhotoSerializer, CommentSerializer, FriendshipSerializer, \
+    PostSerializer, LikeSerializer, TagSerializer, FeedSerializer
+
 
 # Regular view functions
 def profile(req):
     return render(req, "profile.html")
 
+
 def menu(req, pid):
     return render(req, "menu.html", {'pid': pid})
+
 
 def edit_profile(req, pid):
     return render(req, "edit-profile.html",{'pid': pid})
@@ -25,23 +33,61 @@ def messages(req):
 def newsfeed(req):
     return render(req, "newsfeed.html")
 
+
 def reset_password(req, pid):
     return render(req, "reset_password.html", {'pid': pid})
+
 
 def settings(req, pid):
     return render(req, "settings.html", {'pid': pid})
 
+
 def sign_up(req):
     return render(req, "sign up.html")
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('sign_in')  # Redirect to the profile page after sign up
+    else:
+        form = SignUpForm()
+    return render(request, 'sign up.html', {'form': form})
+
 
 def sign_in(req):
     return render(req, "sign_in.html")
 
+
+def sign_in(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('newsfeed')  # Redirect to newsfeed after successful sign-in
+    else:
+        form = AuthenticationForm()
+    return render(request, 'sign_in.html', {'form': form})
+
+
 def forgot_password(req):
     return render(req, "forgotten_password.html")
 
+
 def other_profile(req):
     return render(req, "other_profile.html")
+
 
 # DRF viewsets
 class UserProfileViewSet(viewsets.ModelViewSet):
