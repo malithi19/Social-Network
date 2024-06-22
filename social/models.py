04021ec3ab2d6c94e37f835cd01bc92ae2f35a5b
malithi19 <malithiranawaka8@gmail.com>
2024-06-22 14:53:22 +0530

    Update models.py

--- social/models.py
+++ social/models.py
@@ -25,6 +25,7 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 class Workplace(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
