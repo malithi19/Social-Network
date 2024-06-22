1957806dbc7e8c601dd0de98ca7a0fb6c27dd481
SandaliTharumini <tharuminiwellehewa@gmail.com>
2024-06-22 12:51:29 +0530

    profile linked to django

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -103,7 +103,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'social/static/styles.css'),
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (Uploaded by users)
