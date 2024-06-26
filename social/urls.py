004e6123532c21984967f84bb7d5213666c2114e
A.G.Y.Sanjula <sanjulaagy2001@gmail.com>
2024-06-26 14:43:08 +0530

    update urls.py

--- social/urls.py
+++ social/urls.py
@@ -1,25 +1,25 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from .views import CommentViewSet, current_user
 
 # Regular views
-from .views import create_post
-
 urlpatterns = [
     path('', views.sign_in, name='sign_in'),
     path('profile/', views.profile, name='profile'),
     path('menu/<int:pid>/', views.menu, name='menu'),
     path('', views.edit_profile, name='edit_profile'),
     path('friends/', views.friends, name='friends'),
-    path('logout/', views.logout, name='logout'),
+    path('', views.logout, name='logout'),
     path('messages/', views.messages, name='messages'),
     path('newsfeed/', views.newsfeed, name='newsfeed'),
-    path('create_post/', create_post, name='create_post'),
     path('', views.reset_password, name='reset_password'),
     path('', views.settings, name='settings'),
     path('sign-up/', views.sign_up, name='sign_up'),
+    path('current_user/', current_user, name='current_user'),
     path('forgot-password/', views.forgot_password, name='forgot_password'),
 ]
+
 
 # DRF viewsets
 router = DefaultRouter()
@@ -31,7 +31,6 @@ router.register(r'posts', views.PostViewSet)
 router.register(r'likes', views.LikeViewSet)
 router.register(r'tags', views.TagViewSet)
 router.register(r'feeds', views.FeedViewSet)
-
 urlpatterns += [
     path('api/', include(router.urls)),
 ]
