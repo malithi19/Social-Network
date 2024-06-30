8464d295a72971f8139f14dfe6738f643023212c
SandaliTharumini <tharuminiwellehewa@gmail.com>
2024-06-30 19:51:59 +0530

    imoji visiblity changed on caption

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -65,7 +65,10 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
-    }
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
+    },
 }
 
 # Password validation
