34fcaa3554508726bec761c7cf4135b980101ccc
SandaliTharumini <tharuminiwellehewa@gmail.com>
2024-07-02 12:25:54 +0530

    form done edit profile

--- social/views.py
+++ social/views.py
@@ -15,6 +15,7 @@ from .forms import PostForm
 from django.contrib.auth.models import User
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
+from .forms import UserForm, UserProfileForm
 
 
 # Regular view functions
@@ -232,6 +233,25 @@ def tagable_view(request):
     user_profile = request.user.profile
     friends = user_profile.friends.all()
     return render(request, 'tagable.html', {'friends': friends})
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('profile', user_id=request.user.id)
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = UserProfileForm(instance=request.user.profile)
+
+    return render(request, 'edit_profile.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
 
 
 # DRF viewsets
